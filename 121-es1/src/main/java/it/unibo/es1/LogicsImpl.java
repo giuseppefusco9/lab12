7017ea86cd07a9019f260becd573cbc333eaaee4
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2024-12-09 13:10:17 +0100

    es 121 complete

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,56 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private final int sizeOfBtns;
+	private final List<Integer> ordIntegers;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.sizeOfBtns = Objects.requireNonNull(size);
+		this.ordIntegers = new ArrayList<>();
+		for (int i = 0; i < sizeOfBtns; i++){
+			ordIntegers.add(0);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return Integer.valueOf(this.sizeOfBtns);
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return Collections.unmodifiableList(this.ordIntegers);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.ordIntegers.stream()
+			.map(e -> e < this.sizeOfBtns)
+			.toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		this.ordIntegers.set(elem, this.values().get(elem) + 1);
+		return this.ordIntegers.get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.ordIntegers.stream()
+		.map(String::valueOf)
+		.collect(Collectors.joining("|","<<",">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return this.ordIntegers.stream().allMatch(i -> i == this.values().get(0));
 	}
 }
